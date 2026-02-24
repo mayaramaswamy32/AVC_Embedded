df2fe21d8a7261473aca141c3c53d00169577822
mconter88688 <maddyconter2718@gmail.com>
2026-02-24 06:40:48 -0600

    initial work 2/23

--- Embedded.py
+++ Embedded.py
@@ -16,3 +16,31 @@
 # - getHeight()
 #  - .getRollPitchYaw()[2]
 # - .getValue()
+class Devices:
+    DEFAULT_TIMESTEP = 32
+    LEFT_MOTORS = "left wheel motor"
+    RIGHT_MOTORS = "right wheel motor"
+    CAMERA = "camera"
+    IMU = "inertial unit"
+
+
+class Robot:
+    def __init__(self):
+        self.timestep = Devices.DEFAULT_TIMESTEP
+    def getBasicTimeStep(self):
+        return self.timestep
+    def getDevice(self, name):
+        if name == Devices.LEFT_MOTORS or name == Devices.RIGHT_MOTORS:
+            return Motor(name)
+        elif name == Devices.IMU:
+            return
+        elif name == Devices.CAMERA:
+            return
+        else:
+            raise ValueError("Invalid Device Type")
+
+class Motor:
+    def __init__(self,name):
+        self.name = name
+    def setPosition(position_value: float):
+        pass
